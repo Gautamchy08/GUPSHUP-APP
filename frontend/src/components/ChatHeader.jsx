40c891bab74a8b6ed2411a839ede70f5ecf085e4
gautam chy <gautamchy8789@gmail.com>
2025-06-25 21:24:00 +0530

    profilePic preview

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -1,8 +1,10 @@
 import { X } from 'lucide-react'
+import React, { useState } from 'react'
 import { useAuthStore } from '../store/useAuthStore'
 import { useChatStore } from '../store/useChatStore'
 
 const ChatHeader = () => {
+  const [profilePic, setProfilePic] = useState('/avatar.png')
   const { selectedUser, setSelectedUser } = useChatStore()
   const { onlineUsers } = useAuthStore()
 
@@ -12,12 +14,21 @@ const ChatHeader = () => {
         <div className='flex items-center gap-3'>
           {/* Avatar */}
           <div className='avatar'>
-            <div className='size-10 rounded-full relative'>
+            <button
+              className='size-12 rounded-full overflow-hidden relative '
+              onClick={fullProfilePic =>
+                setProfilePic(
+                  selectedUser.profilePic
+                    ? selectedUser.profilePic
+                    : '/avatar.png'
+                )
+              }
+            >
               <img
                 src={selectedUser.profilePic || '/avatar.png'}
                 alt={selectedUser.fullName}
               />
-            </div>
+            </button>
           </div>
 
           {/* User info */}
@@ -34,6 +45,26 @@ const ChatHeader = () => {
           <X />
         </button>
       </div>
+
+      {profilePic && (
+        <div className='fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-[9999]'>
+          <div className='relative max-w-[90vw] max-h-[90vh]'>
+            <button
+              onClick={() => setProfilePic(false)}
+              className='absolute top-2 right-2 text-white bg-black/60 p-1 rounded-full z-[10000]'
+            >
+              <X size={24} />
+            </button>
+            <div className='w-full h-full flex items-center justify-center'>
+              <img
+                src={profilePic}
+                alt='Full Profile'
+                className='max-w-[95vw] max-h-[95vh] object-contain rounded-xl shadow-xl'
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
